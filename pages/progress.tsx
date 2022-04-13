fa6ba5a9d20509dc8560d28489858f16ae757da1
a2krocks <aakapurv@gmail.com>
2022-04-13 22:28:42 +0530

    Moved img to proper webpage

--- pages/progress.tsx
+++ pages/progress.tsx
@@ -1,16 +1,43 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Progress.module.css'
 
 const Progress: NextPage = () => {
     return (
-        <Image
-            alt='Progress'
-            src='/images/progress.png'
-            layout='fill'
-            objectFit='contain'
-        />)
+
+        /* 
+            <Image
+                alt='Progress'
+                src='/images/progress.png'
+                layout='fill'
+                objectFit='contain'
+                />
+        
+        */
+
+        <div>
+            <Head>
+                <title>A3 Group</title>
+            </Head>
+            <main>
+                <div className={styles.project_container}>
+                    <a className={styles.project}>Project</a>
+                    <a className={styles.project_title}>mínyma opoudípote</a>
+                </div>
+                <div className={styles.project_alt_divider}></div>
+                <a className={styles.project_alt}>どこでもメッセージ</a>
+                <div className={styles.project_arrival_container}>
+                    <a className={styles.project_arrival}>Comming Fall</a>
+                    <a className={styles.project_arrival_year}>2023</a>
+                </div>
+                <div className={styles.project_progress_container} >
+                    <img src='/images/a3_logo.png' height={30} />
+                    <a className={styles.project_progress_link} href='/progress'>https://a3group.co.in/progress</a>
+                </div>
+            </main>
+        </div>
+    )
 }
 
 export default Progress
