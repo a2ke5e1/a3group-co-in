ccd4dada4cd2b11b7223f35898b65486ebf18aa4
= <aakapurv@gmail.com>
2022-10-02 02:37:22 +0530

    Added Yearly Progress Page and Favicon

---
+++ pages/yearly_progress/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <Head>
+      <title>Yearly Progress</title>
+      <link rel="apple-touch-icon" sizes="180x180" href="/yearly_progress/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/yearly_progress/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/yearly_progress/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/yearly_progress/favicon/site.webmanifest"></link>
+    </Head>
+  )
+}
+
+export default Home
