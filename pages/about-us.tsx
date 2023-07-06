cd8bbe5f0028b35fb7472280e0baaae3c7514fe7
a2krocks <aakapurv@gmail.com>
2023-07-06 23:12:21 +0530

    About Us

---
+++ pages/about-us.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'; 
+import Head from 'next/head';
+import Footer from '../components/Footer';
+
+const AboutUs: NextPage =()=> {
+    return (
+        <>
+            <Head>
+                <title>About Us</title>
+            </Head>
+            <main>
+               
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default AboutUs; 
