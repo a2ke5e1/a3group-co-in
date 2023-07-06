e2131c6e814a00b30aa78cd847178b3ba929ebc7
a2krocks <aakapurv@gmail.com>
2023-07-06 22:48:42 +0530

    Fixed Next.js 13 Migration Issues

--- pages/yearly-progress/index.tsx
+++ pages/yearly-progress/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Article from '../../components/Article'
 import styles from '../../styles/yearly-progress/index.module.scss'
 import info from '../../info/yearly-progress.json'
