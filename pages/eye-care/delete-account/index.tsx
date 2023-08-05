b29ea2c676029e8cdfd1d663da851ef8cace4d46
a2krocks <aakapurv@gmail.com>
2023-08-05 20:02:04 +0530

    Fixed Headers

--- pages/eye-care/delete-account/index.tsx
+++ pages/eye-care/delete-account/index.tsx
@@ -9,19 +9,19 @@ const DeletePage: NextPage = () => {
     return (
         <>
           <Head>
-            <title>Eye Care - Delete Account</title>
+            <title>Eye Care - Delete Your Account</title>
             <meta name="description" content="Eye Care - Your Vision Health Companion" />
 
             <meta property="og:url" content="https://www.a3group.co.in/eye-care" />
             <meta property="og:type" content="website" />
-            <meta property="og:title" content="Eye Care" />
+            <meta property="og:title" content="Eye Care - Delete Your Account" />
             <meta property="og:description" content="Eye Care - Your Vision Health Companion" />
-            <meta property="og:image" content="https://www.a3group.co.in/eye-care/images/ss/Screenshot_20221002_014215.png" />
+            <meta property="og:image" content="https://www.a3group.co.in/eye-care/images/ss/Screenshot_1691238815.png" />
     
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content="Eye Care" />
+            <meta name="twitter:title" content="Eye Your Care" />
             <meta name="twitter:description" content="Eye Care - Your Vision Health Companion" />
-            <meta name="twitter:image" content="https://www.a3group.co.in/eye-care/images/ss/Screenshot_20221002_014215.png" />
+            <meta name="twitter:image" content="https://www.a3group.co.in/eye-care/images/ss/Screenshot_1691238836.png" />
     
     
             <link rel="apple-touch-icon" sizes="180x180" href="/eye-care/favicon/apple-touch-icon.png" />
--- pages/eye-care/index.tsx
+++ pages/eye-care/index.tsx
@@ -11,19 +11,19 @@ const EyeCareHome: NextPage = () => {
   return (
     <>
       <Head>
-        <title>{info.name}</title>
-        <meta name="description" content="Yearly Progress is an android app which provides beautiful widgets, which shows the progress of day, week, month and year." />
+        <title>Eye Care</title>
+        <meta name="description" content="Eye Care - Your Vision Health Companion" />
 
         <meta property="og:url" content="https://www.a3group.co.in/eye-care" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Yearly Progress" />
-        <meta property="og:description" content="Yearly Progress is an android app which provides beautiful widgets, which shows the progress of day, week, month and year." />
-        <meta property="og:image" content="https://www.a3group.co.in/eye-care/images/ss/Screenshot_20221002_014215.png" />
+        <meta property="og:title" content="Eye Care" />
+        <meta property="og:description" content="Eye Care - Your Vision Health Companion" />
+        <meta property="og:image" content="https://www.a3group.co.in/eye-care/images/ss/Screenshot_1691238815.png" />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Yearly Progress" />
-        <meta name="twitter:description" content="Yearly Progress is an android app which provides beautiful widgets, which shows the progress of day, week, month and year." />
-        <meta name="twitter:image" content="https://www.a3group.co.in/eye-care/images/ss/Screenshot_20221002_014215.png" />
+        <meta name="twitter:title" content="Eye Care" />
+        <meta name="twitter:description" content="Eye Care - Your Vision Health Companion" />
+        <meta name="twitter:image" content="https://www.a3group.co.in/eye-care/images/ss/Screenshot_1691238836.png" />
 
 
         <link rel="apple-touch-icon" sizes="180x180" href="/eye-care/favicon/apple-touch-icon.png" />
