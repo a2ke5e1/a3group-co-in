51ce3bf482e7d6d0eb2166dfec31a2adb2ac4408
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-01-26 22:54:51 +0530

    Add redirect from root to yearly-progress page

--- next.config.js
+++ next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
     domains: ['play.google.com', 'play-lh.googleusercontent.com'],
   }, 
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  async redirects() { 
+    return [
+      {
+        source: '/', 
+        destination: '/yearly-progress',
+        permanent: true,
+      }
+    ]
+  }, 
 }
 
 module.exports = withMDX(nextConfig)
