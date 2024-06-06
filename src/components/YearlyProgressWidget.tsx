f0726680f9c430a127381eaeb7803501b81f3de7
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-06-06 18:43:46 +0530

    Few UI Changes

--- src/components/YearlyProgressWidget.tsx
+++ src/components/YearlyProgressWidget.tsx
@@ -47,6 +47,23 @@ const YearlyProgressWidget: React.FC<YearlyProgressWidgetProps> = ({
     }
   };
 
+  const styledProgress = () => {
+    const progress = getProgress(type);
+    
+    const progressText = progress.toString().split(".");
+    const progressInteger = progressText[0];
+    const progressDecimal = progressText[1] ?? "00"
+
+    return (
+      <>
+        <span className="font-bold text-2xl">{progressInteger}</span>
+        <span className="font-semibold text-xl">{"."}</span>
+        <span className="font-semibold text-xl">{progressDecimal}</span>
+        <span className="font-semibold text-xl">{"%"}</span>
+      </>
+    )
+  }
+
   return (
     isClient ? <div className="relative bg-blue-50 py-2 px-4 min-w-fit rounded-2xl overflow-hidden text-blue-800">
       <div
@@ -55,7 +72,7 @@ const YearlyProgressWidget: React.FC<YearlyProgressWidgetProps> = ({
       ></div>
       <h2 className="relative text-sm font-light uppercase p-0 m-0">{type}</h2>
       <p className="relative text-lg p-0 ">{getTypeValue(type)}</p>
-      <p className="relative pt-4 font-bold text-2xl">{progress.toString().substring(0, 13)}%</p>
+      <p className="relative pt-4">{styledProgress()}</p>
       <p className="relative text-sm">of {((getEndOfTimeMillis(type) - getStartOfTimeMillis(type)) /  1000).toFixed(0)}s</p>
     </div>: <div></div>
   );
