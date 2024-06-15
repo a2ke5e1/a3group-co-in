8949bae140bd2fd30ca1f776bfec9dc6bd3fd6a4
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-06-16 00:14:31 +0530

    Changed the api to sunrise

--- src/components/YearlyProgressSunsetSunriseWidget.tsx
+++ src/components/YearlyProgressSunsetSunriseWidget.tsx
@@ -1,32 +1,49 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import YearlyProgressWidget from "@/components/YearlyProgressWidget";
+import { parse } from "path";
 
 type YearlyProgressSunsetSunriseWidgetProps = {
   apiKey?: string;
 };
 
 type Coordinates = [number, number];
+type SunsetSunrise = {
+  sunrise: number;
+  sunset: number;
+};
 
 const YearlyProgressSunsetSunriseWidget = ({
   apiKey,
 }: YearlyProgressSunsetSunriseWidgetProps) => {
   const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
   const [weatherData, setWeatherData] = useState<any>(null);
+  const [sunriseSunset, setSunriseSunset] = useState<SunsetSunrise|null>(null); // [sunset, sunrise
   const [loading, setLoading] = useState<boolean>(true);
 
   const convertUnixSeconds = (unix: number) => {
-    const date = new Date(unix * 1000);
+    const date = new Date(unix);
     return date.toLocaleTimeString().toUpperCase();
   };
 
-  const fetchWeatherData = async (lat: number, lon: number) => {
+  // const fetchWeatherData = async (lat: number, lon: number) => {
+  //   const staticData = await fetch(
+  //     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`,
+  //     { cache: "force-cache" }
+  //   );
+  //   return staticData.json();
+  // };
+
+  const fetchSunsetSunrise = async (lat: number, lon: number) => {
+
     const staticData = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`,
+      `https://api.sunrisesunset.io/json?lat=${lat}&lng=${lon}`,
       { cache: "force-cache" }
     );
+
+    
     return staticData.json();
-  };
+  }
 
   useEffect(() => {
     const getLongLat = async (): Promise<Coordinates> => {
@@ -52,9 +69,24 @@ const YearlyProgressSunsetSunriseWidget = ({
         setCoordinates([lat, lon]);
 
         // console.log("Coordinates:", lat, lon);
+        
+        const { results } = await fetchSunsetSunrise(lat, lon);
+        // console.log("Sunset/Sunrise:", results);
 
-        const data = await fetchWeatherData(lat, lon);
-        setWeatherData(data);
+        const { date, sunrise, sunset } = results;
+
+        
+        const sunsetUnix = new Date(`${date} ${sunset}`);
+        const sunriseUnix = new Date(`${date} ${sunrise}`);
+
+        
+
+        setSunriseSunset({ sunrise: sunriseUnix.getTime(), sunset: sunsetUnix.getTime() });
+
+        
+
+        // const data = await fetchWeatherData(lat, lon);
+        // setWeatherData(data);
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
@@ -69,11 +101,15 @@ const YearlyProgressSunsetSunriseWidget = ({
     return <div></div>;
   }
 
-  if (!weatherData) {
+  // if (!weatherData) {
+  //   return <div></div>;
+  // }
+
+  if (!sunriseSunset) {
     return <div></div>;
   }
 
-  const { sunset, sunrise } = weatherData.sys;
+  // const { sunset, sunrise } = weatherData.sys;
 
   return (
     <YearlyProgressWidget
@@ -81,10 +117,12 @@ const YearlyProgressSunsetSunriseWidget = ({
       custom={{
         title: "Daylight",
         description: `Sunrise at ${convertUnixSeconds(
-          sunrise
-        )} and Sunset at ${convertUnixSeconds(sunset)}.`,
-        startTimestamp: sunrise * 1000,
-        endTimestamp: sunset * 1000,
+          sunriseSunset.sunrise
+        )} and Sunset at ${convertUnixSeconds(
+          sunriseSunset.sunset
+        )}.`,
+        startTimestamp: sunriseSunset.sunrise,
+        endTimestamp: sunriseSunset.sunset,
       }}
     />
   );
