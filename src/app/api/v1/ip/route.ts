76bc958653cc2c98f223df3e6442b7b94bb24f87
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-06-19 12:14:05 +0530

    feat: Improve IP retrieval in route.ts
    Refactor the GET function in route.ts to improve IP retrieval. Instead of relying solely on the request.ip property, the code now checks for the 'x-real-ip' header and falls back to the 'x-forwarded-for' header if necessary. This change ensures more accurate IP detection.
    
    Fixes #456

--- src/app/api/v1/ip/route.ts
+++ src/app/api/v1/ip/route.ts
@@ -1,8 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const ip = request.ip;
-    // console.log(ip);
+    
+    const ipHeader = request.headers.get('x-real-ip');
+
+    const ip = ipHeader ? ipHeader : request.headers.get('x-forwarded-for');
+
+    console.log(ipHeader);
+    console.log(ip);
+
     const location = await fetch(`http://ip-api.com/json/${ip}`);
     const locationJson = await location.json();
 
