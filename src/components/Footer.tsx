c7922239ceefb212c55bc87dea7b26d6fc9044e5
a2ke5e1 <aakapurv@gmail.com>
2024-12-06 02:43:25 +0530

    refactor: Update Footer component layout and styling; remove unused footer.tsx file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import styles from "@styles/components/footer.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -18,7 +17,7 @@ export default function Footer({
   return (
     <footer className="bg-surface-container-low px-4 py-8">
       <div className="max-w-screen-2xl mx-auto">
-        <div className="grid md:grid-cols-3 sm:md:grid-cols-2 grid-cols-1 align-top justify-between gap-4">
+        <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:md:grid-cols-2 grid-cols-1 align-top justify-between gap-4">
           <section>
             <Link href={"/"}>
               <Image
@@ -28,41 +27,63 @@ export default function Footer({
                 alt="A3 Group Logo"
               />
             </Link>
-            <p className="my-2 font-light text-sm dark:text-white">
+            <p className="my-2 text-label-medium">
               © 2018 - 2024 A3 Group, All right reserved.
             </p>
           </section>
 
-          <section className="dark:text-white">
-            <p className="text-xs uppercase font-light">JOIN OUR COMMUNITY</p>
-            <Link href={"https://t.me/phycalc"}>Telegram</Link>
+          <section className="">
+            <p className="text-label-small uppercase ">JOIN OUR COMMUNITY</p>
+            <Link
+              href={"https://t.me/phycalc"}
+              className=" my-2 text-body-medium hover:underline"
+            >
+              Telegram
+            </Link>
           </section>
 
           {delete_data_url && (
-            <section className="dark:text-white">
-              <p className="text-xs uppercase font-light">MANAGE YOUR DATA</p>
-              <Link href={delete_data_url}>Delete Your Account</Link>
+            <section className="">
+              <p className="text-label-small uppercase">MANAGE YOUR DATA</p>
+              <Link
+                href={delete_data_url}
+                className=" my-2 text-body-medium hover:underline"
+              >
+                Delete Your Account
+              </Link>
             </section>
           )}
 
           {apps && (
-            <section className="dark:text-white">
-              <p className="text-xs uppercase font-light">Apps</p>
-              <div className="grid grid-cols-1">
-                <Link href="/yearly-progress">Yearly Progress</Link>
-                <Link href="/eye-care">Eye Care</Link>
+            <section className="">
+              <p className="text-label-small uppercase">Apps</p>
+              <div className="grid grid-cols-1  my-2 text-body-medium">
+                <Link href="/yearly-progress" className="hover:underline">
+                  Yearly Progress
+                </Link>
+                <Link href="/eye-care" className="hover:underline">
+                  Eye Care
+                </Link>
               </div>
             </section>
           )}
 
-          <section className="dark:text-white">
-            <p className="text-xs uppercase font-light">Others</p>
-            <div className="grid grid-cols-1">
+          <section className="">
+            <p className="text-label-small uppercase">Others</p>
+            <div className="grid grid-cols-1 my-2 text-body-medium">
               {terms_of_service_url && (
-                <Link href={terms_of_service_url}>Terms of Service</Link>
+                <Link href={terms_of_service_url} className="hover:underline">
+                  Terms of Service
+                </Link>
               )}
-              {policy_url && <Link href={policy_url}>Privacy Policy</Link>}
-              <Link href="/about-us">About Us</Link>
+              {policy_url && (
+                <Link href={policy_url} className="hover:underline">
+                  Privacy Policy
+                </Link>
+              )}
+              <Link href="/about-us" className="hover:underline">
+                About Us
+              </Link>
             </div>
           </section>
         </div>
